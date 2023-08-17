8f85c0456caf1194c3b042ac5b3b18f87d54b9b6
FL1XEvac <Altamira2004@outlook.com>
2023-08-16 23:19:08 -0300

    Ejercicios complementarios terminado

---
+++ Ejercicios complementarios.py
@@ -0,0 +1,53 @@
+"""ejecicios 1,2,3,4"""
+
+numero1=(15)
+numero2=(4.5)
+suma= (numero1+numero2)
+resta=(numero1-numero2)
+multiplicacion=(numero1*numero2)
+division=(numero1/numero2)
+print(suma, resta, multiplicacion, division)
+
+"""ejecicios 5,6,7,8"""
+nombre="Ulises Jimenez"
+precio=(650.5)
+descuento=(0.25)
+precio_final=(precio*descuento)
+precio_final=(precio-precio_final)
+print(precio_final)
+
+"""ejercicios 9,10"""
+cadena="Vive con orgullo. Si te vence tu debilidad, calienta tu corazón, aprieta los dientes y sigue adelante"
+longitud=len(cadena)
+print(longitud)
+
+"""ejercicio 11"""
+precio1=int(650.6)
+print(precio1)
+
+"""ejercicio 12"""
+nombre1= "Ulises"
+apellido= " Jimenez "
+nombre_completo= (nombre1+apellido)
+print(nombre_completo)
+
+"""ejercicio 13"""
+edad=(19)
+nose=(edad+5-10)
+print(nose)
+
+"""ejercicio 14"""
+altura=(1.95)
+altura=(altura*4)
+altura=(altura/3)
+print(altura)
+
+"""ejercicio 15"""
+nombre2= "ULISES JIMENEZ"
+nombre2= nombre2.lower()
+print(nombre2)
+
+"""ejercicio 16"""
+nombre3= "ULISES JIMENEZ"
+nombre3= nombre3.title()
+print(nombre3)
