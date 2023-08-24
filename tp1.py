384042770efb37827dc56ca6d1cc095106910072
FL1XEvac <Altamira2004@outlook.com>
2023-08-24 00:20:31 -0300

    Trabajos practicos

--- tp1.py
+++ tp1.py
@@ -1,3 +1,10 @@
-numeros1= (10,1,5,11)
-operacion=numeros1[3]-numeros1[1]+numeros1[2]+numeros1[0] 
-print(operacion)
+km_en_litro= int(input("Ingrese la cantidad de km puede recorrer con 1 litro de combustible: "))
+tanque= int(input("Ingrese la capacidad de su tanque en litros: "))
+distancia= int(input("Ingrese cuantos kilometros recorreran: "))
+
+distancia1= (distancia//km_en_litro)
+
+tanque_necesarios= (distancia1/tanque)
+
+print("Para completar el viaje necesitaran: ",tanque_necesarios," tanques de combustible")
+
